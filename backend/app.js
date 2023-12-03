2e407360b4eb52b277b97371c90bd9a83f37707f
VishnuDath710 <vishnudathrock@gmail.com>
2023-12-03 20:05:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,12 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 
 app.use(express.json());
