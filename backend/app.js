251601dd58e3187397489f4b65891919b5cb9315
VishnuDath710 <vishnudathrock@gmail.com>
2023-12-03 21:49:34 +0530

    yo

--- backend/app.js
+++ backend/app.js
@@ -17,14 +17,6 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
-
 app.use(express.json());
 app.use(cors());
 app.use('/', authRoutes);
