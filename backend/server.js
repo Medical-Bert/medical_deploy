a5272cbd17e9d8c0c2c25cba45d311a3f0c83a66
VishnuDath710 <vishnudathrock@gmail.com>
2023-12-03 18:43:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
@@ -14,32 +13,31 @@ app.use(cookieParser());
 const allowedOrigins = ['http://localhost:3000', 'https://medical-deploy.vercel.app/'];
 
 app.use(cors({
-  credentials: true,
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+    credentials: true,
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
 }));
 
 app.use(express.json());
 
 const mongoose = require('mongoose');
 mongoose
-  .connect(process.env.REACT_APP_Mongolink)
-  .then(() => {
-    console.log('Connected to MongoDB');
-    const port = 5000;
+    .connect(process.env.REACT_APP_Mongolink, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+        const port = 5000;
 
-    app.use('/', authRoutes);
+        app.use('/', authRoutes);
 
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
     });
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
