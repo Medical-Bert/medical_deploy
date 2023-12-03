6a5cda60b6b53cd5465faaf14ef51481f81d9ed4
VishnuDath710 <vishnudathrock@gmail.com>
2023-12-03 18:57:54 +0530

    y

--- frontend/src/Components/login.js
+++ frontend/src/Components/login.js
@@ -17,7 +17,7 @@ const LoginPage = () => {
     const handleFormSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.get('http://medical-deploy.vercel.app/login', {
+            const response = await axios.get('https://medical-deploy.vercel.app/login', {
                 params: {
                     identifier: identifier,
                     password: password
@@ -27,7 +27,7 @@ const LoginPage = () => {
     
             if (response.data.status === 'success') {
                 // Handle successful login
-                axios.get('http://localhost:5000/profile', { withCredentials: true })
+                axios.get('https://medical-deploy.vercel.app/profile', { withCredentials: true })
                     .then((response) => {
                         const { username } = response.data;
                     })
