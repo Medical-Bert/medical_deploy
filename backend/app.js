9bf626f00d6253c2ed16a89bf2983df505f1d233
VishnuDath710 <vishnudathrock@gmail.com>
2023-12-03 19:54:00 +0530

    uo

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+const authRoutes = require('./api/routes/authRoutes');
+const bodyParser = require('body-parser');
+
+
+
+dotenv.config({ path: './.env' });  
+
+
+const app = express();
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+
+
+
+app.use(express.json());
+app.use(cors());
+app.use('/', authRoutes);
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.REACT_APP_Mongolink, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+app.get('*', (req, res, next) => {
+    res.status(200).json({
+        message: 'bad request'
+    })
+})
+
+    
+
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv');
-const authRoutes = require('./api/routes/authRoutes');
-
-dotenv.config({ path: './.env' });
+const http = require('http');
+const app = require('./app');
+const port = process.env.REACT_APP_PORT|| 5000;
 
 
-
-
-
-
-
-const app = express();
-app.use(cookieParser());
-
-// Allow requests from both http://localhost:3000 and https://medical-deploy.vercel.app/
-app.get('*', (req, res, next) => {
-    res.status(200).json({
-        message: 'bad request'
-    })
-})
-const allowedOrigins = ['https://medical-deploy-frontend.vercel.app/'];
-
-app.use(cors({
-    credentials: true,
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-}));
-
-app.use(express.json());
-
-const mongoose = require('mongoose');
-mongoose
-    .connect(process.env.REACT_APP_Mongolink, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to MongoDB');
-        const port = 5000;
-
-        app.use('/', authRoutes);
-
-        app.listen(port, () => {
-            console.log(`Server running on port ${port}`);
-        });
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
-    });
+const server = http.createServer(app);
+server.listen(port,()=>{console.log('this app is running on '+port)});
