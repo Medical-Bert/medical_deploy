7820ff307fde5aa37ba7eadfe2b5abdc763a5a78
VishnuDath710 <vishnudathrock@gmail.com>
2023-12-07 15:05:08 +0530

    yo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -57,7 +57,7 @@ function App() {
         if (Cookies.get('jwt')) {
             const checkAuthenticationStatus = async () => {
                 try {
-                    const response = await axios.get('http://localhost:5000/profile', { withCredentials: true });
+                    const response = await axios.get('https://medicalbert-api.onrender.com/profile', { withCredentials: true });
                     const { username } = response.data;
                     
                     setLoggedIn(true);
